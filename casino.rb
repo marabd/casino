8d8a05ec3091bc6881c0a5ee4c67e910c7af6551
Mara Behles Dean <maraebehles@gmail.com>
2016-05-16 11:46:57 -0600

    first commit

---
+++ casino.rb
@@ -0,0 +1,17 @@
+#require_relative 'mechanics/dice'
+Dir[File.dirname(__FILE__) + '/mechanics/*.rb'].each {|file| require file }
+
+class Casino
+	include Mechanics
+
+	def play
+		puts Mechanics::Dice.roll
+		cards = Mechanics::Deck.new.cards.shuffle
+		cards.each do |card|
+			puts "#{card.rank} of #{card.suit}"
+		end
+	end
+end
+
+@game = Casino.new
+@game.play
---
+++ mechanics/dice.rb
@@ -0,0 +1,10 @@
+#dice.rb - file should be named as module when using RAILS
+module Mechanics
+	module Dice
+		def self.roll
+	 		die = []
+	 		die << rand(1..6) << rand(1..6) #random dice between 1 & 6 && some random dice between 1 & 6
+ 		end
+	end
+end
+
